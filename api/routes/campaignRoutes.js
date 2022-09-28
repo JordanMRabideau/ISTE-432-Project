7a45eb100e4f889c95cd8e4169cb1717c5b29629
Jordan <jordanmrabideau@gmail.com>
2022-09-28 18:07:21 -0400

    add get and update society routes

--- api/routes/campaignRoutes.js
+++ api/routes/campaignRoutes.js
@@ -7,8 +7,8 @@ module.exports = app => {
         return res.send({message: "Test passsed"})
     })
 
-    router.post("/createSociety", function(req, res) {
-        console.log(req)
+    // Create society
+    router.post("/society", function(req, res) {
         const sql = "INSERT INTO society VALUES (?, ?, ?, ?, ?)";
         const values = [
             req.body.society_id,
@@ -21,8 +21,34 @@ module.exports = app => {
             if (err) throw err;
             console.log("Added a society.")
         })
-
     });
+
+    //Get society
+    router.get("/society", function(req, res) {
+        const sql = "SELECT society.name, society.member_count, society.auth1_name, society.auth2_name FROM society WHERE society_id = ?";
+        const value = req.body.society_id;
+
+        conn.query(sql, value, function(err, result) {
+            if (err) throw err;
+            res.send(result)
+        }) 
+    })
+
+    //Update society
+    router.put("/society", function(req, res) {
+        const sql = "UPDATE society SET society.name = ?, society.member_count = ?, society.auth1_name = ?, society.auth2_name = ? WHERE society_id = ?";
+        const values = [
+            req.body.name,
+            req.body.member_count,
+            req.body.auth1_name,
+            req.body.auth2_name,
+            req.body.society_id,
+        ];
+        conn.query(sql, values, function(err, result) {
+            if (err) throw err;
+            res.send("Updated a society.")
+        })
+    })
 
     app.use('/api', router)
 }
--- server.js
+++ server.js
@@ -17,18 +17,6 @@ app.get('/', function(req, res) {
   return res.send({error: true, message: 'hello'})
 })
 
-// const db = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'root',
-//   password: process.env.PASSWORD,
-//   database: 'book'
-// });
-
-// db.connect(error => {
-//   if (error) throw error;
-//   console.log("Connected to database.")
-// });
-
 require("./api/routes/campaignRoutes.js")(app);
 app.listen(port);
 
