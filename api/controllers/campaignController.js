6b61801412162c955dbc40296ad726d893e98e1e
Jordan <jordanmrabideau@gmail.com>
2022-11-29 21:48:06 -0500

    add voters when creating a new campaign

--- api/controllers/campaignController.js
+++ api/controllers/campaignController.js
@@ -11,108 +11,133 @@ exports.generate_campaign = (req, res) => {
       return res.send(err);
     }
 
-    // Make a query for inserting a new query
-    const insertCampaign = `INSERT INTO campaigns (society_id, name, start_time, end_time, vote_count, active) VALUES (?, ?, ?, ?, ?, ?)`;
-
-    const insertCampaignValues = [
-      society_id,
-      campaign_name,
-      start_time,
-      end_time,
-      0,
-      "N",
-    ];
-
-    // Insert the campaign and get the resulting ID
-    conn.query(
-      insertCampaign,
-      insertCampaignValues,
-      function (error1, result1) {
-        if (error1) {
-          return conn.rollback(() => {
-            return res.send(error1);
-          });
-        }
-
-        const campaign_id = result1.insertId;
-
-        const insertBallotQuestions = `INSERT INTO ballot_questions (campaign_id, question, maximum_selections, question_placement) VALUES ?`;
-
-        const insertQuestionsValues = req.body.questions.map((question) => {
-          return [
-            campaign_id,
-            question.title,
-            question.limit,
-            question.position,
-          ];
+    //Get the list of members in the current society
+    const getMembers = `SELECT member_id FROM members WHERE society_id = ?`
+    conn.query(getMembers, [society_id], function(error1, members) {
+      if (error1) {
+        return conn.rollback(() => {
+          return res.send(error1);
         });
+      }
 
-        conn.query(
-          insertBallotQuestions,
-          [insertQuestionsValues],
-          function (error2, result2) {
-            if (error2) {
-              return conn.rollback(() => {
-                return res.send(error2);
-              });
-            }
+      // Make a query for inserting a new campaign
+      const insertCampaign = `INSERT INTO campaigns (society_id, name, start_time, end_time, vote_count, active) VALUES (?, ?, ?, ?, ?, ?)`;
 
-            // this will determine the ids of the newly generated questions to be used for choice insertion
-            const questionIds = [];
-            for (
-              let i = result2.insertId;
-              i < result2.insertId + result2.affectedRows;
-              i++
-            ) {
-              questionIds.push(i);
-            }
+      const insertCampaignValues = [
+        society_id,
+        campaign_name,
+        start_time,
+        end_time,
+        0,
+        "N",
+      ];
 
-            const insertBallotChoices = `INSERT INTO choices (campaign_id, question_id, name, bio, image_filepath, vote_count, choice_placement) VALUES ?`;
-
-            let insertChoicesValues = [];
-
-            req.body.questions.forEach((question, questionIndex) => {
-              const newRow = [campaign_id, questionIds[questionIndex]];
-              const choiceInfo = question.choices.map((choice) => {
-                return [
-                  choice.name,
-                  choice.info,
-                  choice.image,
-                  0,
-                  choice.position,
-                ];
-              });
-              choiceInfo.forEach((choiceRow) => {
-                insertChoicesValues.push(newRow.concat(...choiceRow));
-              });
-              console.log(insertChoicesValues);
+      // Insert the campaign and get the resulting ID
+      conn.query(
+        insertCampaign,
+        insertCampaignValues,
+        function (error2, result1) {
+          if (error2) {
+            return conn.rollback(() => {
+              return res.send(error2);
             });
+          }
 
+          // use the campaign id to insert voters
+          const campaign_id = result1.insertId;
+          const insertVoters = `INSERT INTO campaign_voters (member_id, campaign_id, voted) VALUES ?`
+          const voterValues = members.map((member) => {
+            return [member.member_id, campaign_id, "N"]
+          })
+
+          conn.query(insertVoters, [voterValues], function(error3, result2) {
+            if (error3) {
+              return conn.rollback(() => {
+                return res.send(error3);
+              });
+            }
+
+            // Insert questions
+            const insertBallotQuestions = `INSERT INTO ballot_questions (campaign_id, question, maximum_selections, question_placement) VALUES ?`;
+            const insertQuestionsValues = req.body.questions.map((question) => {
+              return [
+                campaign_id,
+                question.title,
+                question.limit,
+                question.position,
+              ];
+            });
+  
             conn.query(
-              insertBallotChoices,
-              [insertChoicesValues],
-              function (error3, result3) {
-                if (error3) {
+              insertBallotQuestions,
+              [insertQuestionsValues],
+              function (error4, result3) {
+                if (error4) {
                   return conn.rollback(() => {
-                    return res.send(error3);
+                    return res.send(error4);
                   });
                 }
-
-                conn.commit(function (commitError) {
-                  if (commitError) {
-                    return conn.rollback(function () {
-                      return res.send({ commitError });
+  
+                // this will determine the ids of the newly generated questions to be used for choice insertion
+                const questionIds = [];
+                for (
+                  let i = result3.insertId;
+                  i < result3.insertId + result3.affectedRows;
+                  i++
+                ) {
+                  questionIds.push(i);
+                }
+  
+                const insertBallotChoices = `INSERT INTO choices (campaign_id, question_id, name, bio, image_filepath, vote_count, choice_placement) VALUES ?`;
+  
+                let insertChoicesValues = [];
+  
+                req.body.questions.forEach((question, questionIndex) => {
+                  const newRow = [campaign_id, questionIds[questionIndex]];
+                  const choiceInfo = question.choices.map((choice) => {
+                    return [
+                      choice.name,
+                      choice.info,
+                      choice.image,
+                      0,
+                      choice.position,
+                    ];
+                  });
+                  choiceInfo.forEach((choiceRow) => {
+                    insertChoicesValues.push(newRow.concat(...choiceRow));
+                  });
+                  console.log(insertChoicesValues);
+                });
+  
+                conn.query(
+                  insertBallotChoices,
+                  [insertChoicesValues],
+                  function (error5, result4) {
+                    if (error5) {
+                      return conn.rollback(() => {
+                        return res.send(error5);
+                      });
+                    }
+  
+                    conn.commit(function (commitError) {
+                      if (commitError) {
+                        return conn.rollback(function () {
+                          return res.send({ commitError });
+                        });
+                      }
+  
+                      return res.send(result4);
                     });
                   }
-
-                  return res.send(result3);
-                });
+                );
               }
             );
-          }
-        );
-      }
-    );
+
+          })
+        }
+      );
+
+    })
   });
 };
 
